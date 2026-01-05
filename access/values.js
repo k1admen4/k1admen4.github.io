fbd53b80ee0f9ffcac9903b2c151ead4189384dc
k1admen4 <ustanovkaclashmini@gmail.com>
2026-01-05 01:21:00 +0100

    Update values.js

--- access/values.js
+++ access/values.js
@@ -3,15 +3,15 @@ var isRightsEnabled = true; // Права
 var isCovidCertificateEnabled = true; // COVID-сертификат
 
 // Общее
-var birthdate = "28.02.2007"; // Дата рождения
-var fullname = "Булмала Александра Сергіївна" // ФИО
-var name = "Александра" // Имя
+var birthdate = "26.07.2007"; // Дата рождения
+var fullname = "Рішан Інна Романівна" // ФИО
+var name = "Інна" // Имя
 
 // Паспорт
-var passport_id = "1024903" // Номер паспорта
+var passport_id = "010940844" // Номер паспорта
 
 // КПП
-var kpp_id = "4010635952" // ИНН
+var kpp_id = "5040652704" // ИНН
 
 // Права
 var rights_valid_until = "01.01.2027" // Права "Дійсні до"
@@ -21,4 +21,5 @@ var rights_id = "KBE968639" // Права "Серія та номер""
 
 // COVID-сертификат
 var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
+
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
